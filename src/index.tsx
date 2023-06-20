77167a24e97d4df2f88d91c8ee2e5c1ad2daa8a6
ashu8912 <ashughildiyal5@gmail.com>
2023-06-20 13:19:44 +0530

    update with new gadgetstreamingservice

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import {DetailsViewSectionProps, registerDetailsViewSection} from '@kinvolk/headlamp-plugin/lib';
+import {DetailsViewSectionProps, K8s, registerDetailsViewSection} from '@kinvolk/headlamp-plugin/lib';
 import SectionBox from "@kinvolk/headlamp-plugin/lib/CommonComponents/SectionBox";
 import React, { useState, useEffect } from 'react';
 import SimpleTable from "@kinvolk/headlamp-plugin/lib/CommonComponents/SimpleTable";
@@ -9,129 +9,129 @@ import SimpleTable from "@kinvolk/headlamp-plugin/lib/CommonComponents/SimpleTab
 // import { K8s } from '@kinvolk/headlamp-plugin/lib/K8s';
 // import { Typography } from '@material-ui/core';
 
-let websocket = null;
-let gadgets = {};
-const listeners = new Map();
-let id = 0;
 
-function publish(gadgetID, data) {
-    if (listeners.has(gadgetID)) {
-        listeners.get(gadgetID).forEach(callback => callback(data));
-    }
+const IG_CONTAINER_KEY = 'k8s-app';
+const IG_CONTAINER_VALUE = 'gadget';
+
+
+
+function isIGPod(podResource) {
+    return podResource.metadata.labels[IG_CONTAINER_KEY] === IG_CONTAINER_VALUE;
 }
 
-function addListener(gadgetID, callback) {
-    console.log('new listener for', gadgetID);
-    if (!listeners.has(gadgetID)) {
-        listeners.set(gadgetID, []);
-    }
-    listeners.get(gadgetID).push(callback);
-}
-
-function removeListener(gadgetID, callback) {
-    console.log('remove listener for', gadgetID);
-    if (listeners.has(gadgetID)) {
-        const channelListeners = listeners.get(gadgetID);
-        const index = channelListeners.indexOf(callback);
-        if (index !== -1) {
-            channelListeners.splice(index, 1);
-        }
-        if (channelListeners.length === 0) {
-            let gadget = gadgets[gadgetID];
-            if (gadget) {
-                websocket.send(JSON.stringify({ action: 'stop', payload: { id: gadget.id } }));
-                delete(gadgets[gadgetID]);
-            }
-            listeners.delete(gadgetID);
-        }
-    }
-}
-
-function createWebSocketURL() {
-    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-    const hostname = window.location.hostname;
-    const port = 4466 || window.location.port;
-    // TODO: this URL has to be built with information on how IG can be accessed (mainly the pod name on the specific
-    //  node that should deliver the data for a given pod). This simple example will always connect to the same gadget
-    //  pod and might only be able to deliver data from pods on the same node. We will most likely be adding
-    //  a fanning-out mechanism to IG later on so that the requests could be automatically be forwarded to the
-    //  correct nodes.
-    const path = '/proxy/ws?proxyInfo=' + encodeURIComponent('ws://gadget.gadget-rfwlb.gadget.pod.minikube-docker/cmd/?cmd=%5B%22%2Fusr%2Fbin%2Fsocat%22%2C%22%2Frun%2Fgadgetwebservice.socket%22%2C%22-%22%5D');
-    return `${protocol}//${hostname}${port ? `:${port}` : ''}${path}`;
-}
-
-function initWebSocket() {
-    if (!websocket) {
-        const url = createWebSocketURL();
-        console.log('connecting to', url);
-        websocket = new WebSocket(url);
-
-        websocket.addEventListener('open', () => {
-            console.log('WebSocket connection opened');
-        });
-
-        websocket.addEventListener('message', event => {
-            const data = JSON.parse(event.data);
-            console.log('event', data.id, data.payload);
-            publish(data.id, data.payload);
-        });
-
-        websocket.addEventListener('close', () => {
-            console.log('WebSocket connection closed');
-            websocket = null;
-        });
-
-        websocket.addEventListener('error', error => {
-            console.error('WebSocket error:', error);
-        });
-    }
-}
-
-// This version doesn't actually implement filtering for the Pod that is shown. Those things could be filled out as
-// params in gadget.params (e.g. `{ "podname": "abc" }`). This could also be combined with the catalog to fill out
-// most of such params automatically, depending on the context.
-function runGadget(gadget) {
-    console.log('running gadget', gadget);
-
-    gadget.id = ''+(++id);
-
-    initWebSocket();
-
-    if (websocket.readyState === WebSocket.OPEN) {
-        websocket.send(JSON.stringify({ action: 'start', payload: gadget }));
-    } else {
-        websocket.addEventListener('open', () => {
-            websocket.send(JSON.stringify({ action: 'start', payload: gadget }));
-        }, { once: true }); // Automatically remove the event listener after it's called once
+function parseJsonFromText(text) {
+    try {
+        return JSON.parse(text);
+    } catch (e) {
+        console.error('error parsing JSON', e);
     }
 
-    gadgets[gadget.id] = gadget;
-    return gadget.id;
+    return null;
 }
 
-// Right now, this example doesn't use the gadget catalog to automatically infer the column information for the tables;
-// instead those are hardcoded. Using the catalog would make this reusable for any gadget with this kind of output.
 registerDetailsViewSection(({ resource }: DetailsViewSectionProps) => {
     if (!resource || resource.kind !== 'Pod') return null;
 
+    const decoder = new TextDecoder('utf-8');
     const [entries, setEntries] = useState([]);
+    const [pods, error] = K8s.ResourceClasses.Pod.useList();
+    const gadgetID = "socket-gadget-snapshot"
+    const [igPod, setIGPod] = useState(null);
+    const execRef = React.useRef(null);
+    const [textStream, setTextStream] = useState("");
+   
+    function runGadgetWithActionAndPayload(socket, action, payload) {
+        if(!action) {
+            socket.send('\0'+JSON.stringify({ payload }) + "\n")
+            return
+        }
+        socket.send('\0'+JSON.stringify({ action, payload }) + "\n");
+    }
 
     useEffect(() => {
-        const id = runGadget({ gadgetName: 'socket', gadgetCategory: 'snapshot' });
+        if (!pods) {
+            return
+        }
+        const igPod = pods?.find(isIGPod);
+        if (!igPod) {
+            return
+        }
+        setIGPod(igPod)
+    }, [pods])
+    
 
-        const listenerCallback = (data) => {
-            console.log('got data', data);
-            setEntries(data);
-        };
+    useEffect(() => {
+        if (!igPod) {
+            return
+        }
 
-        addListener(id, listenerCallback);
+        if(execRef.current) {
+            return
+        }
+        execRef.current = igPod.exec('gadget', (items) => {
+          const text = decoder.decode(items.slice(1));
 
-        // Clean up the listener when the component is unmounted
+          if (new Uint8Array(items)[0] !== 1) {
+            return;
+          }
+
+          const textSplitBasedOnNewLine = text.split('\n')
+            if(textSplitBasedOnNewLine.length > 1) {
+                textSplitBasedOnNewLine.forEach((textPart, index) => {
+                    if(index === 0) {
+                        setTextStream(oldTextStream => {
+                            // if the last parsed ended with \n start fresh
+                            if(oldTextStream.endsWith('\n')) {
+                                return textPart
+                            }
+                            return oldTextStream + textPart
+                        })
+                    } else {
+                        setTextStream(textPart)
+                    }
+                })
+            } else if(text.endsWith('\n')) {
+              const updatedText = text.slice(0, -1)
+              setTextStream(oldTextStream => {
+                // if the last parsed ended with \n start fresh
+                if(oldTextStream.endsWith('\n')) {
+                    return updatedText
+                }
+                return oldTextStream + updatedText
+            })
+            } else {
+              setTextStream(oldTextStream => oldTextStream + text)
+            }
+        }, {
+            command: ["/usr/bin/socat", "/run/gadgetstreamingservice.socket", "-"],
+            tty: false,
+            stdin: true,
+            stdout: true,
+            stderr: false,
+        })
+
+        const socket = execRef.current.getSocket()
+
+        socket.addEventListener('open', () => runGadgetWithActionAndPayload(socket, 'start', { gadgetName: 'socket', gadgetCategory: "snapshot", id: gadgetID }))
+
         return () => {
-            removeListener(id, listenerCallback);
-        };
-    }, [resource])
+            socket.removeEventListener('open', () => runGadgetWithActionAndPayload(socket, 'start', { gadgetName: 'socket', gadgetCategory: 'snapshot', id: gadgetID }))
+            execRef.current.cancel()
+        }
+    }, [resource, igPod])
 
+    useEffect(() => {
+        const jsonParts = parseJsonFromText(textStream)
+        if(jsonParts) {
+                if(Array.isArray(jsonParts) && jsonParts.length > 0 && jsonParts[0].id === gadgetID) {
+                    const payloadMap = jsonParts.map((part) => part.payload)
+                    setEntries(entries => [...entries, ...payloadMap])    
+                } else {
+                    const payloads = jsonParts.payload
+                    setEntries([...payloads])
+                }
+        }
+    }, [textStream])
 
     return (
         <SectionBox title="Sockets">
@@ -163,23 +163,106 @@ registerDetailsViewSection(({ resource }: DetailsViewSectionProps) => {
 
 registerDetailsViewSection(({ resource }: DetailsViewSectionProps) => {
     if (!resource || resource.kind !== 'Pod') return null;
-
+    
+    const decoder = new TextDecoder('utf-8');
     const [entries, setEntries] = useState([]);
+    const [pods, error] = K8s.ResourceClasses.Pod.useList();
+    const gadgetID = "trace-gadget-open-sockets"
+    const [igPod, setIGPod] = useState(null);
+    const execRef = React.useRef(null);
+    const [textStream, setTextStream] = useState("");
+
+    function runGadgetWithActionAndPayload(socket, action, payload) {
+        socket.send('\0'+JSON.stringify({ action, payload }) + "\n");
+    }
 
     useEffect(() => {
-        const id = runGadget({ gadgetName: 'open', gadgetCategory: 'trace' });
+        if (!pods) {
+            return
+        }
+        const igPod = pods?.find(isIGPod);
+        if (!igPod) {
+            return
+        }
+        setIGPod(igPod)
+    }, [pods])
+    
+    
+    useEffect(() => {
+        if (!igPod) {
+            return
+        }
 
-        const listenerCallback = (data) => {
-            console.log('trace open', data);
-            setEntries((prev) => [data, ...prev.slice(0,99)]);
-        };
-        addListener(id, listenerCallback);
+        if(execRef.current) {
+            return
+        }
+        execRef.current = igPod.exec('gadget', (items) => {
+            const text = decoder.decode(items.slice(1));
 
-        // Clean up the listener when the component is unmounted
+            if (new Uint8Array(items)[0] !== 1) {
+              return;
+            }
+            const textSplitBasedOnNewLine = text.split('\n')
+            if(textSplitBasedOnNewLine.length > 1) {
+                textSplitBasedOnNewLine.forEach((textPart, index) => {
+                    if(index === 0) {
+                        setTextStream(oldTextStream => {
+                            // if the last parsed ended with \n start fresh
+                            if(oldTextStream.endsWith('\n')) {
+                                return textPart
+                            }
+                            return oldTextStream + textPart
+                        })
+                    } else {
+                        setTextStream(textPart)
+                    }
+                })
+            } else if(text.endsWith('\n')) {
+              const updatedText = text.slice(0, -1)
+              setTextStream(oldTextStream => {
+                // if the last parsed ended with \n start fresh
+                if(oldTextStream.endsWith('\n')) {
+                    return updatedText
+                }
+                return oldTextStream + updatedText
+            })
+            } else {
+              setTextStream(oldTextStream => oldTextStream + text)
+            }
+        }, {
+            command: ["/usr/bin/socat", "/run/gadgetstreamingservice.socket", "-"],
+            tty: false,
+            stdin: true,
+            stdout: true,
+            stderr: false,
+        })
+
+        const socket = execRef.current.getSocket()
+
+        socket.addEventListener('open', () => runGadgetWithActionAndPayload(socket, "start", { gadgetName: 'open', gadgetCategory: 'trace', id: gadgetID }))
+
         return () => {
-            removeListener(id, listenerCallback);
-        };
-    }, [resource])
+            socket.removeEventListener('open', () => runGadgetWithActionAndPayload(socket, "stop", { gadgetName: 'open', gadgetCategory: 'trace', id: gadgetID }))
+            execRef.current.cancel()
+        }
+    }, [resource, igPod])
+
+    useEffect(() => {
+        const jsonParts = parseJsonFromText(textStream)
+        if(jsonParts) {
+                if(Array.isArray(jsonParts) && jsonParts.length > 0 && jsonParts[0].id === gadgetID) {
+                    const payloadMap = jsonParts.map((part) => part.payload)
+                    setEntries(entries => [...entries, ...payloadMap])    
+                } else {
+                    const payloads = jsonParts.payload
+                    if(Array.isArray(payloads) && payloads.length > 0) {
+                        setEntries(entries => [...entries, ...payloads])
+                    } else {
+                        setEntries([...entries, payloads])
+                    }
+                }
+        }
+    }, [textStream])
 
 
     return (
@@ -203,7 +286,7 @@ registerDetailsViewSection(({ resource }: DetailsViewSectionProps) => {
                         getter: e => e.ret
                     }
                 ]}
-                data={entries}
+                data={pods === null ? null : entries}
                 reflectInURL="files"
             />
         </SectionBox>
