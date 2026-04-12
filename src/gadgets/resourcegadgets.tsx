5d72ee7bb73a9ba98047ed8ca1b296cae3d9497b
Parship Chowdhury <parshipchowdhury@gmail.com>
2026-04-12 15:47:32 +0000

    lint fix
    Signed-off-by: Parship Chowdhury <parshipchowdhury@gmail.com>

--- src/gadgets/resourcegadgets.tsx
+++ src/gadgets/resourcegadgets.tsx
@@ -17,7 +17,7 @@ import { HEADLAMP_KEY, HEADLAMP_METRIC_UNIT, HEADLAMP_VALUE, IS_METRIC } from '.
 import { MetricChart } from '../common/MetricChart';
 import { isIGPod } from './helper';
 import usePortForward from './igSocket';
-import { AllColumnMeta, processGadgetData, getSortedColumns } from './utility';
+import { AllColumnMeta, getSortedColumns, processGadgetData } from './utility';
 
 function getGadgetPodForThisResourceNode(node, pods) {
   if (!node || !pods) return null;
